a880f9f690cf0cc474ad663439f64816d0abbf13
vikashkohli7827 <sunil9027290@gmail.com>
2025-08-22 09:18:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TEAMPURVI/ALPHA_MUSIC",
+    "https://github.com/vikashkohli7827/ALPHA_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
